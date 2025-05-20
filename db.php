1c282b6c6e9b7d7661626038e33fbef05f3cc403
Jubair Jull Karnine <83826871+JubairKar9@users.noreply.github.com>
2025-05-21 00:47:07 +0600

    db.php

--- db.php
+++ db.php
@@ -1,1 +1,14 @@
 
+<?php
+$host = 'localhost';
+$db   = 'classroom_system';
+$user = 'root';
+$pass = ''; // Your MySQL password
+$charset = 'utf8mb4';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
