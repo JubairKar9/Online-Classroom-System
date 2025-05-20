33752ec0b6664db400ba908a2b9bc7c8b4468b89
Jubair Jull Karnine <83826871+JubairKar9@users.noreply.github.com>
2025-05-21 00:45:13 +0600

    database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,15 @@
+CREATE DATABASE classroom_system;
+
+USE classroom_system;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('student', 'teacher', 'admin') DEFAULT 'student'
+);
+
+-- Sample User (Password: 12345)
+INSERT INTO users (username, password, role)
+VALUES ('jubair', SHA2('12345', 256), 'teacher');
 
